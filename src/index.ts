695db34f37933ef779a512a33bdbc502d69e004c
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-12-18 13:34:57 +0330

    feat: add ORM and setup DB

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+
+  await orm.getMigrator().up();
+
+//   const post = orm.em.create(Post, { title: "first post" });
+//   await orm.em.persistAndFlush(post);
+
+//   const posts = await orm.em.find(Post, {});
+//   console.log(posts)
+};
+
+main().catch((err) => {
+  console.log(err);
+});
