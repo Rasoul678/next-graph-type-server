ab828fa3aafbb7e67157ad0fadb5350b3b666f75
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-01-23 18:43:22 +0330

    feat: update post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -121,21 +121,25 @@ export class PostResolver {
 
   //! Update a post.
   @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
   async updatePost(
     @Arg("id", () => Int) id: number,
-    @Arg("title", () => String, { nullable: true }) title: string
+    @Arg("title") title: string,
+    @Arg("text") text: string,
+    @Ctx() { req }: MyContext
   ): Promise<Post | null> {
-    const post = await Post.findOne(id);
+    const result = await getConnection()
+      .createQueryBuilder()
+      .update(Post)
+      .set({ text, title })
+      .where('id = :id and "creatorId" = :creatorId', {
+        id,
+        creatorId: req.session.userId,
+      })
+      .returning("*")
+      .execute();
 
-    if (!post) {
-      return null;
-    }
-
-    if (typeof title !== undefined) {
-      await Post.update({ id }, { title });
-    }
-
-    return post;
+    return result.raw[0];
   }
 
   //! Delete a post.
